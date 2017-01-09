167256a7134394894bd11a3de9286ad01f753639
paq <89paku@gmail.com>
2017-01-09 17:03:01 +0900

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,24 @@ fn help() {
 }
 
 fn textconv(excel_file_path: &String) {
-    let mut excel:Excel = Excel::open(excel_file_path).expect("The provided file was not an readable Excel file");
-    
+    let mut excel: Excel = Excel::open(excel_file_path)
+        .expect("The provided file was not an readable Excel file");
+
     for sheet_name in excel.sheet_names().unwrap() {
         let sheet = excel.worksheet_range(&sheet_name).unwrap();
         for row in sheet.rows() {
             let mut cells: Vec<String> = Vec::new();
-            
+
             for cell in row {
                 let str: String = datatype_to_str(&cell)
                     .replace("\\", "\\\\")
                     .replace("\n", "\\n")
                     .replace("\r", "\\r")
                     .replace("\t", "\\t");
-                
+
                 cells.push(str);
             }
-            
+
             println!("[{}] {}", sheet_name, cells.join("\t"));
         }
     }
@@ -37,16 +38,18 @@ fn datatype_to_str(data_type: &DataType) -> String {
         DataType::Int(value) => value.to_string(),
         DataType::Float(value) => value.to_string(),
         DataType::String(ref value) => value.clone(),
-        DataType::Error(ref error) => match *error {
-            CellErrorType::Div0 => "#DIV/0!".to_string(),
-            CellErrorType::NA => "#N/A".to_string(),
-            CellErrorType::Name => "#NAME?".to_string(),
-            CellErrorType::Null => "#NULL!".to_string(),
-            CellErrorType::Num => "#NUM!".to_string(),
-            CellErrorType::Ref =>"#REF!".to_string(),
-            CellErrorType::Value => "#VALUE!".to_string(),
-            CellErrorType::GettingData => "#GETTING_DATA".to_string(),
-        }, 
+        DataType::Error(ref error) => {
+            match *error {
+                CellErrorType::Div0 => "#DIV/0!".to_string(),
+                CellErrorType::NA => "#N/A".to_string(),
+                CellErrorType::Name => "#NAME?".to_string(),
+                CellErrorType::Null => "#NULL!".to_string(),
+                CellErrorType::Num => "#NUM!".to_string(),
+                CellErrorType::Ref => "#REF!".to_string(),
+                CellErrorType::Value => "#VALUE!".to_string(),
+                CellErrorType::GettingData => "#GETTING_DATA".to_string(),
+            }
+        } 
         DataType::Empty => "".to_string(),
     };
 }
